bdb3790f4301ac9ea6ad649d0ab826d791fd0911
fedefenner <fedefenner@gmail.com>
2023-11-28 23:44:08 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+//alert("Hola este es mi Javascript");
+
+//const nombre = "holalalalala";
+
+//alert(nombre);
+
+
+
+let menu_responsive = document.querySelector(".checkbtn");
+menu_responsive.onclick = function () {
+    navBar = document.querySelector(".navbar");
+    navBar.classList.toggle("active");
+};
+
